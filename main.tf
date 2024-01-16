766f041e2429bffa7d68b03bb034f5d7e02cf117
Afroz <afrozalikhan81@gmail.com>
2024-01-16 15:42:46 +0530

    remote state

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "params" {
   count = length(var.params)
   name  = var.params[count.index].name
   type  = var.params[count.index].type
-  value = var.params[count.index].value
+  value = var.params[count.index].type
 }
 
 variable "params" {
